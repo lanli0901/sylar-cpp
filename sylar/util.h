8fff66ef4ccdc702f973ef85832e378b14a89705
lanli0901 <1731480512@qq.com>
2025-01-06 15:55:47 +0800

    util_26

--- sylar/util.h
+++ sylar/util.h
@@ -7,11 +7,16 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <vector>
+#include <string>
 
 namespace sylar {
 
 pid_t GetThreadID();
 uint32_t GetFiberID();
+
+void Backtrace(std::vector<std::string>& bt, int size, int skip = 1);
+std::string BacktraceToString(int size, int skip = 2, const std::string& prefix = "");
 
 }
 
